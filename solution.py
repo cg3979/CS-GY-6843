4fb0b27d0cf12f9fc7dec6dee589d586f0070135
cg3979 <112892532+cg3979@users.noreply.github.com>
2022-09-24 16:17:20 -0400

    Update solution.py

--- solution.py
+++ solution.py
@@ -21,7 +21,7 @@ def webServer(port=13331):
         connectionSocket, addr = serverSocket.accept()  # Fill in start -are you accepting connections?     #Fill in end
 
         try:
-            message = connectionSocket.recv(1024).decode  # Fill in start -a client is sending you a message   #Fill in end
+            message = connectionSocket.recv(1024).decode()  # Fill in start -a client is sending you a message   #Fill in end
             filename = message.split()[1]
 
             # opens the client requested file.
@@ -61,5 +61,5 @@ def webServer(port=13331):
     # sys.exit()  # Terminate the program after sending the corresponding data
 
 
-if __name__ == "__main__":
-    webServer(13331)
+##if __name__ == "__main__":
+    ##webServer(13331)
